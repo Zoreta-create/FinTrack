625a94109373986f7b84fd00a92b737c806131eb
Gabriel <gabrieldejesus01.gdj@gmail.com>
2026-03-22 01:25:25 -0300

    feat: criando o UserRepository

--- fintrack/src/main/java/com/gabriel/fintrack/User/UserRepository.java
+++ fintrack/src/main/java/com/gabriel/fintrack/User/UserRepository.java
@@ -1,4 +1,11 @@
 package com.gabriel.fintrack.User;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository <UserModel, Long> {
+    boolean existsByEmail(String email); //verifica se existe no banco de dados
+
+    Optional <UserModel> findByEmail(String email); //busca por email no banco de dados
 }
