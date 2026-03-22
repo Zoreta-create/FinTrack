a0e4f8bb6d964c83a013b7f1398947cefa0b0aa6
Gabriel <gabrieldejesus01.gdj@gmail.com>
2026-03-22 00:53:42 -0300

    feat: criando o modelo do Banco de Dados.

---
+++ fintrack/src/main/java/com/gabriel/fintrack/User/UserModel.java
@@ -0,0 +1,33 @@
+package com.gabriel.fintrack.User;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity //Entity transforma uma classe em uma entidade no banco de dados
+@Table(name = "register_user")
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data //serve para reduzir codigo repetitivo (boilerplate) e tornar o codigo mais limpo e focado na logica de negocios
+public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "confirm_password")
+    private String confirmPassword;
+}
+
