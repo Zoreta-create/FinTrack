093d404a3c718c534336c2d8ff275bb73e00b635
Gabriel <gabrieldejesus01.gdj@gmail.com>
2026-03-23 00:25:32 -0300

    refactor: trocando o nome da classe de User para Register.

--- fintrack/src/main/java/com/gabriel/fintrack/Register/RegisterController.java
+++ fintrack/src/main/java/com/gabriel/fintrack/Register/RegisterController.java
@@ -1,12 +1,29 @@
-package com.gabriel.fintrack.User;
+package com.gabriel.fintrack.Register;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.gabriel.fintrack.Register.DTO.RegisterDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
 @CrossOrigin("*")
-public class UserController {
-    
+public class RegisterController {
+
+    @Autowired
+    private RegisterService registerService;
+
+    //ENDPOINT CADASTRO - requisição com o front-end
+    @PostMapping("/signup")
+    public ResponseEntity<?> register (@RequestBody RegisterDTO dto) {
+        try {
+            RegisterModel registerUser = registerService.register(dto);
+            return ResponseEntity.ok(Map.of("mensagem", "Usuário cadastrado com sucesso."));
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
 }
