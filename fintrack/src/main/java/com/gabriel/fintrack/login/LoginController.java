4b64ddc87f375f42253b9720154eb0c8e4a9fc49
Gabriel de Jesus <gabrieldejesus01.gdj@gmail.com>
2026-04-01 17:27:34 -0300

    feat: criando o controller de login

--- fintrack/src/main/java/com/gabriel/fintrack/login/LoginController.java
+++ fintrack/src/main/java/com/gabriel/fintrack/login/LoginController.java
@@ -1,4 +1,28 @@
 package com.gabriel.fintrack.login;
 
+import com.gabriel.fintrack.login.dto.LoginDTO;
+import com.gabriel.fintrack.register.RegisterModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin("*")
 public class LoginController {
+
+    @Autowired
+    private LoginService loginService;
+
+    //ENDPOINT LOGIN
+    @PostMapping("/login")
+    public ResponseEntity<?> login (@RequestBody LoginDTO dto) {
+        try {
+            RegisterModel loginUser = loginService.login(dto);
+            return ResponseEntity.ok("Login realizado com sucesso!");
+        } catch (Exception e) {
+            return ResponseEntity.status(401).body(e.getMessage());
+        }
+    }
+
 }
