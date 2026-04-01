978a09e31ed1c317a7f732a3a10d7b74debcf331
Gabriel de Jesus <gabrieldejesus01.gdj@gmail.com>
2026-04-01 17:29:27 -0300

    feat: criando o DTO de login

--- fintrack/src/main/java/com/gabriel/fintrack/login/dto/LoginDTO.java
+++ fintrack/src/main/java/com/gabriel/fintrack/login/dto/LoginDTO.java
@@ -1,4 +1,13 @@
 package com.gabriel.fintrack.login.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class LoginDTO {
+    private String email;
+    private String password;
 }
