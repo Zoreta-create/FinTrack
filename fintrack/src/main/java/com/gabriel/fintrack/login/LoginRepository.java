5d1d77ffa30b0310bbf3baeb85e8d0e98e9df32f
Gabriel de Jesus <gabrieldejesus01.gdj@gmail.com>
2026-04-01 17:28:14 -0300

    feat: criando o repository

--- fintrack/src/main/java/com/gabriel/fintrack/login/LoginRepository.java
+++ fintrack/src/main/java/com/gabriel/fintrack/login/LoginRepository.java
@@ -1,4 +1,15 @@
 package com.gabriel.fintrack.login;
 
-public interface LoginRepository {
+import com.gabriel.fintrack.register.RegisterModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface LoginRepository extends JpaRepository<RegisterModel, Long> {
+
+    boolean existsByEmail(String email); //verifica se existe no banco de dados
+    Optional<RegisterModel> findByEmail(String email); //busca por email no banco de dados
+
 }
